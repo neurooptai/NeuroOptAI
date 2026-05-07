9659ced6d1984e52007e735017bf08c063c19ca2
neurooptai <neurooptai@gmail.com>
2026-05-07 13:36:43 +0200

    Create __init__.py

---
+++ neuro_opt/metrics/__init__.py
@@ -0,0 +1,11 @@
+from .efficiency_metrics import (
+    compute_efficiency_score,
+    compute_cost_benefit_ratio,
+    compute_training_stability,
+)
+
+__all__ = [
+    "compute_efficiency_score",
+    "compute_cost_benefit_ratio",
+    "compute_training_stability",
+]
