9615d8dd65345974c94364743d6def90c6881b87
neurooptai <neurooptai@gmail.com>
2026-05-07 13:40:19 +0200

    Create __init__.py

---
+++ neuro_opt/utils/__init__.py
@@ -0,0 +1,11 @@
+# neuro_opt/utils/__init__.py
+
+"""
+Utility functions for NeuroOptAI.
+
+This module contains helper functions, logging utilities,
+data formatting tools, and reusable support methods
+used across the framework.
+"""
+
+from .helpers import *
